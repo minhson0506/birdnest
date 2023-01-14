f792df253fabbdb786eb97d490a7e33e1634ef44
Son Dang <minhson0506@gmail.com>
2023-01-14 19:36:28 +0200

    add ui for testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [pilots, setPilots] = useState([
+    {
+      id: 1,
+      pilotName: "A",
+      pilotEmail: "a@gmail.com",
+      pilotPhone: "0123",
+      distance: 200,
+    },
+    {
+      id: 2,
+      pilotName: "B",
+      pilotEmail: "b@gmail.com",
+      pilotPhone: "1234",
+      distance: 100,
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // display list of pilot
+    <div>
+      <h3>Pilots near the birdnest</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>No</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone number</th>
+            <th>Closest distance</th>
+          </tr>
+        </thead>
+        <tbody>
+          {pilots &&
+            pilots.map((pilot) => (
+              <tr key={pilot.id}>
+                <td>{pilot.id}</td>
+                <td>{pilot.pilotName}</td>
+                <td>{pilot.pilotEmail}</td>
+                <td>{pilot.pilotPhone}</td>
+                <td>{pilot.distance}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </div>
   );
 }
